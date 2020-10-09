357198adb638daed60fffe34fdf9166a75a5f62a
tdb <tbrow69@msn.com>
2020-10-08 17:55:46 -0700

    updated javascript, got timer working, on click working

--- javascript.js
+++ javascript.js
@@ -1,74 +1,82 @@
-//declare criteria variables as arrays
-var length = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "123", "124", "125", "126", "127", "128"];
-var characterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var characterUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var characterNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
-var characterSpecial = ["!,", "#", "$", "%", "&", "'", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", ", "{", "|", "}", "~", "]", "^", "_"];
+var startButton = document.getElementById("start");
+var timerButton = document.getElementById("timer");
 
-//declare variables that will take inputs - wrap inside function
-//evaluations of if input is correct
-var userLength = 0
+var secondsLeft = 30;
 
-while(userLength < 8 || userLength > 128) {
-  userLength = prompt("How many characters would you like, between 8-128?");
-}
-alert("Your password will be " + userLength + " characters long");
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timerButton.textContent = secondsLeft + " test";
 
-var userCharacterLower = confirm("Do you want Lower case letters?");
-var userCharacterUpper = confirm("Do you want Upper case letters?");
-var userCharacterNumber = confirm("Do you want Numbers?");
-var userCharacterSpecial = confirm("Do you want Special characters?");
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
 
-// Assignment Code
-//acceptedCharacters.concat(characterLower); 
-//console.log(acceptedCharacters.concat(characterLower));
-
-var generateBtn = document.querySelector("#generate");
-
-function generatePassword() {
-var acceptedCharacters=[]
-var password=""
-
-  if (userCharacterLower === true) {
-    acceptedCharacters=acceptedCharacters.concat(characterLower)
-  }
-  
-
-  if (userCharacterUpper === true) {
-    acceptedCharacters=acceptedCharacters.concat(characterUpper)
-    
-  }
-
-  if (userCharacterNumber === true) {
-    acceptedCharacters=acceptedCharacters.concat(characterNumber)
-    
-  }
-
-  if (userCharacterSpecial === true) {
-    acceptedCharacters=acceptedCharacters.concat(characterSpecial)
-
-  }
-
-  for (var x = 0; x < userLength; x++) {
-    var rand = Math.floor(Math.random() * acceptedCharacters.length);
-    password+=acceptedCharacters[rand];
-    console.log(password, rand);
-  }
-    return password
+  }, 1000);
 }
 
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword, {
+function sendMessage() {
+  timerButton.textContent = "Time Expired";
+}
 
-  });
+setTime();
+    
+//startEl.addEventListener("click", function(){
+  //setTime();
+//}
 
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+    
+    
 
-    passwordText.value = password;
+     
 
-    //prompt(password)
-  }
+    //prompt("questions");
+    //reset("score to 0")
+    //}
 
+    //Timer to 0
+    // Alert time expired, Prompt Initials
+
+    //All questions answered
+    // Alert quiz complete, Prompt Initials
+
+    //Submit button resets everything
+
+    //declare variables as arrays
+    //var correctAnswers1 = (correctAnswer1);
+    //var incorrectAnswers1 = [incorrectAnswer11, incorrectAnswer12, incorrectAnswer13];
+
+    //var correctAnswers2 = (correctAnswer2);
+    //var incorrectAnswers2 = [incorrectAnswer21, incorrectAnswer22, incorrectAnswer23];
+
+    //var correctAnswers3 = (correctAnswer3);
+    //var incorrectAnswers3 = [incorrectAnswer31, incorrectAnswer32, incorrectAnswer33];
+
+    //var correctAnswers4 = (orrectAnswer4);
+    //var incorrectAnswers4 = [incorrectAnswer41, incorrectAnswer42, incorrectAnswer43];
+
+
+    //if (correctAnswers === true) {
+
+      //alert("Correct");
+      //alert(Score + 1)
+      //prompt("next question");
+    //}
+
+    //else {
+      //alert("Incorrect");
+      //decrement 5 seconds
+      //prompt("same question");
+
+    //};
+    //var questionOne = ["The boy walked to the park", "Girl likes to play golf", "Man", "Woman"];
+    //for (key in questionOne) {
+      //$(".question-1")[key].textContent = questionOne[key];
+    //}
+
+    //$(".question-1").on("click", function () {
+      //console.log($(this).text())
+    //})
+  //})
+//}
